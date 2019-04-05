861ff9a1cc8b3e7134cbce7d0074a0e309b12953
Dmytro Halushko <atlassian2Galun>
2019-04-05 16:34:02 +0300

    Create maven project with parent project and one module

---
+++ telegram_bot/src/main/java/kpi/acts/appz/bot/Bot.java
@@ -0,0 +1,21 @@
+package kpi.acts.appz.bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Bot extends TelegramLongPollingBot{
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}
